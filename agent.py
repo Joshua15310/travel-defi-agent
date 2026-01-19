a135987fe889fd99de1a0796420738b37d450ae2
Joshua15310 <odogun3@.com>
2026-01-19 22:28:38 +0100

    Fix f-string syntax error at line 1229

--- agent.py
+++ agent.py
@@ -1226,7 +1226,7 @@ def consultant_node(state: AgentState):
     
     context = ""
     if state.get("flights"):
-        context = f"User viewing flights: {[f'{f['airline']} {f['flight_number']}' for f in state['flights'][:5]]}"
+        context = f"User viewing flights: {[f\"{f['airline']} {f['flight_number']}\" for f in state['flights'][:5]]}"
     elif state.get("hotels"):
         context = f"User viewing hotels: {[h['name'] for h in state['hotels'][:5]]}"
     
